1617ba74c6ab564745dc35363f33f3d023a2722c
Mayur Dhaka <jhonmathews@gmail.com>
2019-10-26 12:46:04 +0530

    add watchOS target

---
+++ Dental-Hygiene/Dental Hygiene Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dental Hygiene Extension
+//
+//  Created by Mayur Dhaka on 26/10/19.
+//  Copyright © 2019 Mayur Dhaka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
