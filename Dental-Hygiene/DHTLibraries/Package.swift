6600dec08123821d5de93be25f78dec8e171b142
Mayur Dhaka <jhonmathews@gmail.com>
2020-03-28 11:29:47 +0530

    Re-add DHTTimer to swiftpackage

--- Dental-Hygiene/DHTLibraries/Package.swift
+++ Dental-Hygiene/DHTLibraries/Package.swift
@@ -14,16 +14,16 @@ let package = Package(
         .library(
             name: "DHTAccess",
             targets: ["DHTAccess"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(
+            name: "DHTTimer",
+            targets: ["DHTAccess"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DHTAccess",
+            dependencies: []),
+        .target(
+            name: "DHTTimer",
             dependencies: []),
         .testTarget(
             name: "DHTAccessTests",
