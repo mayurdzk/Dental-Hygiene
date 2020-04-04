8b7cc2521083372ee5d0b5d52971e595d7aace41
Mayur Dhaka <jhonmathews@gmail.com>
2020-04-04 09:09:21 +0530

    Refactor start brushing into custom view

--- Dental-Hygiene/Dental Hygiene Extension/ContentView.swift
+++ Dental-Hygiene/Dental Hygiene Extension/ContentView.swift
@@ -5,64 +5,15 @@
 //  Created by Mayur Dhaka on 26/10/19.
 //  Copyright © 2019 Mayur Dhaka. All rights reserved.
 //
-
-import Combine
-import DHTAccess
-import HealthKit
 import SwiftUI
-import DHTTimer
 
 struct ContentView: View {
-    @State private var brushingTime = SmallTime()
-    @State private var isUserBrushing: Bool = false
-    private let healthStore = DHTAccess(hkStore: HKHealthStore())
-    private let timer: DHTTimer = DHTTimer()
-    
     var body: some View {
         VStack {
-            Text(brushingTime.displayString())
-            Button(action: {
-                self.isUserBrushing.toggle()
-                if self.isUserBrushing {
-                    self.startBrushingTeeth()
-                } else {
-                    self.stopBrushingTeeth()
-                }
-            }, label: { Text(
-                // TODO: Use localisation
-                isUserBrushing ? "Stop" : "Start"
-                )
-            })
-            #if DEBUG
-            Button(action: {
-                let result = self.healthStore.logToothbrushEventEndedNow(goingOnFor: try! SmallTime(timeInterval: 120.0))
-                .map { (_) -> Void in
-                    print("Done")
-                }.mapError { (e) -> Error in
-                    print(e)
-                    return e
-                }
-                print(result)
-            }, label: { Text("2mins") })
-            #endif
+            StartBrushingView()
         }
     }
     
-    private func startBrushingTeeth() {
-        timer.start {(t) in
-            self.brushingTime = t
-        }
-    }
-    
-    private func stopBrushingTeeth() {
-        logToothbrushEvent()
-        timer.stop()
-        brushingTime = SmallTime()
-    }
-    
-    private func logToothbrushEvent() {
-        _ = healthStore.logToothbrushEventEndedNow(goingOnFor: brushingTime)
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Dental-Hygiene/Dental Hygiene Extension/Views/StartBrushingView.swift
@@ -0,0 +1,72 @@
+//
+//  StartBrushingView.swift
+//  Dental Hygiene Extension
+//
+//  Created by Mayur Dhaka on 04/04/20.
+//  Copyright © 2020 Mayur Dhaka. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+import DHTAccess
+import HealthKit
+import DHTTimer
+
+struct StartBrushingView: View {
+    @State private var brushingTime = SmallTime()
+    @State private var isUserBrushing: Bool = false
+    private let healthStore = DHTAccess(hkStore: HKHealthStore())
+    private let timer: DHTTimer = DHTTimer()
+
+    var body: some View {
+        VStack {
+            Text(brushingTime.displayString())
+            Button(action: {
+                self.isUserBrushing.toggle()
+                if self.isUserBrushing {
+                    self.startBrushingTeeth()
+                } else {
+                    self.stopBrushingTeeth()
+                }
+            }, label: { Text(
+                // TODO: Use localisation
+                isUserBrushing ? "Stop" : "Start"
+                )
+            })
+            #if DEBUG
+            Button(action: {
+                let result = self.healthStore.logToothbrushEventEndedNow(goingOnFor: try! SmallTime(timeInterval: 120.0))
+                .map { (_) -> Void in
+                    print("Done")
+                }.mapError { (e) -> Error in
+                    print(e)
+                    return e
+                }
+                print(result)
+            }, label: { Text("2mins") })
+            #endif
+        }
+    }
+    
+    private func startBrushingTeeth() {
+        timer.start {(t) in
+            self.brushingTime = t
+        }
+    }
+    
+    private func stopBrushingTeeth() {
+        logToothbrushEvent()
+        timer.stop()
+        brushingTime = SmallTime()
+    }
+    
+    private func logToothbrushEvent() {
+        _ = healthStore.logToothbrushEventEndedNow(goingOnFor: brushingTime)
+    }
+}
+
+struct StartBrushingView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartBrushingView()
+    }
+}
